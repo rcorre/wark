8244a5d5e5c7799bfca905e55ba02dee24c89253
Ryan Roden-Corrent <ryan@rcorre.net>
2017-05-30 15:43:30 -0400

    Receive websocket events

--- wark.py
+++ wark.py
@@ -1,21 +1,99 @@
+import os
+import json
+import uuid
 import time
 import shlex
+
 import weechat
+import requests
 from ciscosparkapi import CiscoSparkAPI
+from ws4py.client.threadedclient import WebSocketClient
 
 
 SCRIPT_NAME = "spark"
 FULL_NAME = "plugins.var.python.{}".format(SCRIPT_NAME)
-
+SPARK_SOCKET_URL = 'https://wdm-a.wbx2.com/wdm/api/v1/devices'
 
 weechat.register(SCRIPT_NAME, "rcorre", "0.1", "MIT", "Spark Client", "", "")
 
-
 weechat.prnt("", "Initializing Spark plugin...")
+
 api = CiscoSparkAPI()
 rooms = {room.title: room for room in api.rooms.list()}
-weechat.prnt("", "Spark plugin initialized!")
 
+# Cisco Spark has a websocket interface to listen for message events
+# It isn't documented, I found it here:
+# https://github.com/marchfederico/ciscospark-websocket-events
+class EventListener(WebSocketClient):
+    def __init__(self):
+        spec = {
+            "deviceName": "weechat",
+            "deviceType": "DESKTOP",
+            "localizedModel": "python2",
+            "model": "python2",
+            "name": "weechat",
+            "systemName": "weechat",
+            "systemVersion": "0.1"
+        }
+
+        self.bearer = 'Bearer ' + os.getenv("SPARK_ACCESS_TOKEN")
+        self.headers = {'Authorization': self.bearer}
+
+        resp = requests.post(SPARK_SOCKET_URL, headers=self.headers, json=spec)
+        if resp.status_code != 200:
+            print("Failed to register device {}: {}".format(name, resp.json()))
+        info = resp.json()
+
+        self.dev_url = info['url']
+        super(EventListener, self).__init__(
+            info['webSocketUrl'], protocols=['http-only', 'chat'])
+
+    def opened(self):
+        # authentication handshake
+        self.send(json.dumps({
+            'id': str(uuid.uuid4()),
+            'type': 'authorization',
+            'data': { 'token': self.bearer }
+        }))
+
+    def closed(self, code, reason=None):
+        resp = requests.delete(self.dev_url, headers=self.headers)
+        if resp.status_code != 200:
+            print("Failed to unregister websocket device from Spark")
+
+    def received_message(self, m):
+        try:
+            j = json.loads(str(m))
+        except:
+            print("Failed to parse message {}".format(m))
+            return
+
+        timestamp = j['timestamp']
+        data = j['data']
+        name = data.get('actor', {}).get('displayName')
+        ev = data['eventType']
+
+        if ev == 'status.start_typing':
+            print('{} started typing'.format(name))
+        elif ev == 'status.stop_typing':
+            print('{} stopped typing'.format(name))
+        elif ev == 'conversation.activity':
+            act = data['activity']
+            verb = act['verb']
+            if verb == 'post':
+                msg = api.messages.get(act['id'])
+                print('{} says {}'.format(name, msg))
+        else:
+            print('Unknown event {}'.format(ev))
+
+print('Initializing event listener')
+listener = EventListener()
+print('Connecting event listener')
+listener.connect()
+print('Running event listener')
+listener.run_forever()
+
+weechat.prnt("", "Spark plugin initialized!")
 
 class CommandException(Exception):
     pass
