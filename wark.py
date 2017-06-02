00a148559d3d0d3278ae97af2fa6108d67a99f85
Ryan Roden-Corrent <ryan@rcorre.net>
2017-06-02 16:21:13 -0400

    Start modularizing code

--- wark.py
+++ wark.py
@@ -1,7 +1,6 @@
 import os
 import json
 import uuid
-import time
 import shlex
 
 import weechat
@@ -18,13 +17,40 @@ SPARK_SOCKET_URL = 'https://wdm-a.wbx2.com/wdm/api/v1/devices'
 api = None
 listener = None
 rooms = None
+buffers = []
+
+
+def unixtime(msg):
+    """Get the unix timestamp from a spark message object"""
+    t = time.strptime(msg.created, '%Y-%m-%dT%H:%M:%S.%fZ')
+    return int(time.mktime(t))
+
+
+class Buffer():
+    """Represents a weechat buffer connected to a spark room."""
+
+    def __init__(self, buf, room, api):
+        self.buf = buf
+        self.room = room
+        self.api = api
+
+    def show(self, msg):
+        """Display a message in the buffer."""
+        weechat.prnt_date_tags(self.buf, unixtime(msg), "", msg.text)
+
+    def send(self, txt):
+        """Send a message to the room."""
+        self.api.messages.create(roomId=self.room.id, markdown=txt)
 
 
 # Cisco Spark has a websocket interface to listen for message events
 # It isn't documented, I found it here:
 # https://github.com/marchfederico/ciscospark-websocket-events
 class EventListener(WebSocketClient):
-    def __init__(self):
+    """Listens to the cisco spark web socket."""
+
+    def __init__(self, buffers):
+        self.buffers = buffers
         spec = {
             "deviceName": "weechat",
             "deviceType": "DESKTOP",
@@ -75,15 +101,17 @@ class EventListener(WebSocketClient):
         ev = data['eventType']
 
         if ev == 'status.start_typing':
-            print('{} started typing'.format(name))
+            weechat.prnt('', '{} started typing'.format(name))
         elif ev == 'status.stop_typing':
-            print('{} stopped typing'.format(name))
+            weechat.prnt('', '{} stopped typing'.format(name))
         elif ev == 'conversation.activity':
             act = data['activity']
             verb = act['verb']
             if verb == 'post':
                 msg = api.messages.get(act['id'])
-                print('{} says {}'.format(name, msg))
+                for buf in self.buffers:
+                    if buf.room.id == msg.roomId:
+                        buf.show(msg)
         else:
             print('Unknown event {}'.format(ev))
 
@@ -98,27 +126,31 @@ def buffer_input_cb(data, buf, input_data):
 
 
 def buffer_close_cb(data, buf):
+    """Called on closing a buffer."""
     return weechat.WEECHAT_RC_OK
 
 
 def room_list(buf):
+    """Print a list of visible rooms."""
     weechat.prnt(buf, '--Rooms--')
     weechat.prnt(buf, '\n'.join(rooms.keys()))
     weechat.prnt(buf, '---------')
 
 
 def room_open(buf, name):
+    """Open a new buffer connected to a spark room."""
     room = rooms[name]
     newbuf = weechat.buffer_new("spark." + room.title, "buffer_input_cb", "",
                                 "buffer_close_cb", "")
-    def unixtime(msg):
-        t = time.strptime(msg.created, '%Y-%m-%dT%H:%M:%S.%fZ')
-        return int(time.mktime(t))
+    buffers[room.id] = Buffer(buf, room, api)
 
-    messages = api.messages.list(roomId=room.id)
-    for msg in sorted(messages, key=unixtime):
-        text = msg.text.encode('ascii', 'replace') if msg.text else ''
-        weechat.prnt_date_tags(newbuf, unixtime(msg), "", text)
+
+def rehistory(_buf):
+    #messages = api.messages.list(roomId=room.id)
+    #for msg in sorted(messages, key=unixtime):
+    #    text = msg.text.encode('ascii', 'replace') if msg.text else ''
+    #    weechat.prnt_date_tags(newbuf, unixtime(msg), "", text)
+    pass
 
 
 COMMANDS = {
@@ -144,6 +176,13 @@ def spark_command_cb(data, buf, command):
         return weechat.WEECHAT_RC_ERROR
 
 
+weechat.register(SCRIPT_NAME, "rcorre", "0.1", "MIT", "Spark Client", "", "")
+api = CiscoSparkAPI()
+rooms = {room.title: room for room in api.rooms.list()}
+listener = EventListener()
+listener.connect()
+
+
 weechat.hook_command(
     # Command name and description
     'spark', '',
@@ -157,10 +196,3 @@ weechat.hook_command(
     '|'.join(COMMANDS.keys()),
     # Function name
     'spark_command_cb', '')
-
-
-weechat.register(SCRIPT_NAME, "rcorre", "0.1", "MIT", "Spark Client", "", "")
-api = CiscoSparkAPI()
-rooms = {room.title: room for room in api.rooms.list()}
-listener = EventListener()
-listener.connect()
